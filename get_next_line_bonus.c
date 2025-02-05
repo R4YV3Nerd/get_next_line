23de4935c0eb5a0dd4de058af28b0752de7267d5
maitoumg <marvin@42.fr>
2025-02-05 08:03:35 +0100

    norm updated

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,50 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maitoumg <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/05 08:02:52 by maitoumg          #+#    #+#             */
+/*   Updated: 2025/02/05 08:02:55 by maitoumg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line_bonus.h"
 
 #ifndef FDS_MAX
 # define FDS_MAX 1024
 #endif
 
-static int read_and_clean(int fd, char *line, char **buffer)
+static int	read_and_clean(int fd, char *line, char **buffer)
 {
-    int ret;
+	int	ret;
 
-    ret = read(fd, line, (size_t)BUFFER_SIZE);
-    if (ret < 0)
-    {
-        free(*buffer);
-        return (-1);
-    }
-    if (ret == 0)
-        return (0);
-    line[ret] = '\0';
-    while (ret > 0)
-    {
-        *buffer = ft_strjoin(*buffer, line);
-        if (ft_clean(line) > 0)
-            break ;
-        ret = read(fd, line, (size_t)BUFFER_SIZE);
-        line[ret] = '\0';
-    }
-    return (ret);
+	ret = read(fd, line, (size_t)BUFFER_SIZE);
+	if (ret < 0)
+	{
+		free(*buffer);
+		return (-1);
+	}
+	if (ret == 0)
+		return (0);
+	line[ret] = '\0';
+	while (ret > 0)
+	{
+		*buffer = ft_strjoin(*buffer, line);
+		if (ft_clean(line) > 0)
+			break ;
+		ret = read(fd, line, (size_t)BUFFER_SIZE);
+		line[ret] = '\0';
+	}
+	return (ret);
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    static char line[FDS_MAX][BUFFER_SIZE + 1];
-    char *buffer;
+	static char	line[FDS_MAX][BUFFER_SIZE + 1];
+	char		*buffer;
 
-    if (fd < 0 || fd >= FDS_MAX || BUFFER_SIZE < 1)
-        return (NULL);
-    buffer = ft_strjoin(0, line[fd]);
-    if (ft_clean(line[fd]) > 0)
-        return (buffer);
-    if (read_and_clean(fd, line[fd], &buffer) <= 0)
-    {
-        if (buffer && *buffer != '\0')
-            return (buffer);
-        free(buffer);
-        return (NULL);
-    }
-    return (buffer);
+	if (fd < 0 || fd >= FDS_MAX || BUFFER_SIZE < 1)
+		return (NULL);
+	buffer = ft_strjoin(0, line[fd]);
+	if (ft_clean(line[fd]) > 0)
+		return (buffer);
+	if (read_and_clean(fd, line[fd], &buffer) <= 0)
+	{
+		if (buffer && *buffer != '\0')
+			return (buffer);
+		free(buffer);
+		return (NULL);
+	}
+	return (buffer);
 }
-
--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -1,62 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maitoumg <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/05 08:02:39 by maitoumg          #+#    #+#             */
+/*   Updated: 2025/02/05 08:02:42 by maitoumg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line_bonus.h"
-#include <string.h>
 
-char *ft_strjoin(char *s1, char *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
-    char *joined;
-    int i;
-    int j;
+	char	*joined;
+	int		i;
+	int		j;
 
-    if (s2[0] == '\0')
-        return (0);
-    joined = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-    if (!joined)
-        return (0);
-    i = 0;
-    while (s1 && s1[i])
-    {
-        joined[i] = s1[i];
-        i++;
-    }
-    j = 0;
-    while (s2 && s2[j] && s2[j] != '\n')
-        joined[i++] = s2[j++];
-    if (s2[j] == '\n')
-        joined[i++] = '\n';
-    joined[i] = '\0';
-    free(s1);
-    return (joined);
+	if (s2[0] == '\0')
+		return (0);
+	joined = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!joined)
+		return (0);
+	i = 0;
+	while (s1 && s1[i])
+	{
+		joined[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2 && s2[j] && s2[j] != '\n')
+		joined[i++] = s2[j++];
+	if (s2[j] == '\n')
+		joined[i++] = '\n';
+	joined[i] = '\0';
+	free(s1);
+	return (joined);
 }
 
-size_t ft_strlen(char *s)
+size_t	ft_strlen(char *s)
 {
-    size_t i;
+	size_t	i;
 
-    i = 0;
-    while (s && s[i] && s[i] != '\n')
-        i++;
-    if (s && s[i] == '\n')
-        i++;
-    return (i);
+	i = 0;
+	while (s && s[i] && s[i] != '\n')
+		i++;
+	if (s && s[i] == '\n')
+		i++;
+	return (i);
 }
 
-int ft_clean(char *str)
+int	ft_clean(char *str)
 {
-    int i;
-    int j;
-    int check;
+	int	i;
+	int	j;
+	int	check;
 
-    i = 0;
-    j = 0;
-    check = 0;
-    while (str[i])
-    {
-        if (check)
-            str[j++] = str[i];
-        if (str[i] == '\n')
-            check = 1;
-        str[i++] = '\0';
-    }
-    return (check);
+	i = 0;
+	j = 0;
+	check = 0;
+	while (str[i])
+	{
+		if (check)
+			str[j++] = str[i];
+		if (str[i] == '\n')
+			check = 1;
+		str[i++] = '\0';
+	}
+	return (check);
 }
-
